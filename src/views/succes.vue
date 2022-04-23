2820f7509059418dd1a28e2ab8be952d1de4be5b
kevinG73 <kevinguelade@gmail.com>
2022-04-23 16:51:58 +0200

    page de success et d'echec

--- src/views/succes.vue
+++ src/views/succes.vue
@@ -1,28 +1,85 @@
 <template>
   <div class="container">
-    <!-- section  -->
-    <section class="row mb-5 pb-md-4 align-items-center">
-      <div class="col-md-5">
-        <div class="masthead-followup-icon d-inline-block mb-2 text-white bg-danger">
+    <div class="row">
+      <div class="col-md-6 mx-auto mt-5 mb-5">
+        <div class="payment">
+          <div class="payment_header">
+            <div class="check">
+              <i class="bi bi-check-circle"></i>
+            </div>
+          </div>
+          <div class="content">
+            <h1>Paiement effectué avec succès !</h1>
+            <p>Vous recevrez bientôt un mail concernant les dernières étapes à suivre pour recevoir votre commande </p>
+            <router-link to="/">Retourner sur la page d'accueil</router-link>
+          </div>
 
         </div>
-        <h2 class="display-5 fw-normal">Succès</h2>
-        <p>
-          great <br>
-        </p>
       </div>
-      <div class="col-md-7 ps-md-5">
-        <img class="img-fluid mt-3 mx-auto" src="https://getbootstrap.com/docs/5.0/assets/img/bootstrap-themes.png"
-             alt="Bootstrap Themes" width="700" height="500" loading="lazy">
-      </div>
-    </section>
+    </div>
   </div>
 </template>
 
+<style>
+.payment {
+  border: 1px solid #f2f2f2;
+  height: 280px;
+  border-radius: 20px;
+  background: #fff;
+  margin-bottom: 5rem;
+}
+
+.payment_header {
+  background: rgba(255, 102, 0, 1);
+  padding: 20px;
+  border-radius: 20px 20px 0 0;
+
+}
+
+.check {
+  margin: 0 auto;
+  width: 50px;
+  height: 50px;
+  border-radius: 100%;
+  background: #fff;
+  text-align: center;
+}
+
+.check i {
+  vertical-align: middle;
+  line-height: 50px;
+  font-size: 30px;
+}
+
+.content {
+  text-align: center;
+}
+
+.content h1 {
+  font-size: 25px;
+  padding-top: 25px;
+}
+
+.content a {
+  width: 200px;
+  height: 35px;
+  color: #fff;
+  border-radius: 30px;
+  padding: 5px 10px;
+  background: rgba(255, 102, 0, 1);
+  transition: all ease-in-out 0.3s;
+}
+
+.content a:hover {
+  text-decoration: none;
+  background: #000;
+}
+
+</style>
 <script lang="ts">
 import {defineComponent, ref} from "vue";
 
 export default defineComponent({
-  name: "Home"
+  name: "success"
 });
 </script>
